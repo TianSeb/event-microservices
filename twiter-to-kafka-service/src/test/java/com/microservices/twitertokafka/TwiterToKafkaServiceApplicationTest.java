57f2a78a22241ac49851b688132de74c8de0494b
AlassTian <sebastianalasia@kinandcarta.com>
2023-11-06 01:42:03 -0300

    Init Commit

---
+++ twiter-to-kafka-service/src/test/java/com/microservices/twitertokafka/TwiterToKafkaServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.microservices.twitertokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwiterToKafkaServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
